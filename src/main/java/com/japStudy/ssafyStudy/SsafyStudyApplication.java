b54509819d960bdda78ee34e7d28f46eb7dac4a9
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2025-02-03 00:08:54 +0900

    feat: project base setting
    - start.spring.io에서 다운받아 설치
    - gradle, java 17, h2 등 의존성

---
+++ src/main/java/com/japStudy/ssafyStudy/SsafyStudyApplication.java
@@ -0,0 +1,13 @@
+package com.japStudy.ssafyStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsafyStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsafyStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/japStudy/ssafyStudy/SsafyStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.japStudy.ssafyStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsafyStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
