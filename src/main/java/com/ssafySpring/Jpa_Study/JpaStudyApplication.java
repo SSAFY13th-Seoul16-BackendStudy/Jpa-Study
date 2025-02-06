53c3e2fb993644d8429edcf51064a229de4f7081
Nacho-Cola <sugy1030@gmail.com>
2025-02-07 02:33:41 +0900

    feat:View Setting

---
+++ src/main/java/com/ssafySpring/Jpa_Study/JpaStudyApplication.java
@@ -0,0 +1,14 @@
+package com.ssafySpring.Jpa_Study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaStudyApplication.class, args);
+	}
+
+}
+
